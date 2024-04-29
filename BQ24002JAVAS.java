bda25d7eda04aa2fa35415c3ed92d8147f402ec6
BQ24002UES <167727918+VIAJEROMISTERIOSO@users.noreply.github.com>
2024-04-28 19:54:37 -0600

    Update BQ24002JAVAS.java
    THIS IS A MESSAGE FIX

--- BQ24002JAVAS.java
+++ BQ24002JAVAS.java
@@ -7,5 +7,5 @@ public class HelloWorld {
 // HolaMundo.java
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundos");
+        System.out.println("Hola Mundo Jurásico");
     }
